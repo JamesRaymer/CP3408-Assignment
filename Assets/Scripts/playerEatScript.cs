86bd22701f8a72599807bd4e81f7adfc5a843a7f
Justin <Justin.peever@my.jcu.edu.au>
2020-05-05 22:07:00 +1000

    var storage, eating script start and levelscript to scenescript rename

---
+++ Assets/Scripts/playerEatScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerEatScript : MonoBehaviour
+
+
+{
+
+    public Text scoreTxt;
+    int en01score = 0;
+    int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get scores for each enemy type
+        en01score = PlayerPrefs.GetInt("Enemy01score");
+        print("Enemy score: " + en01score);
+        // reset score in UI
+        scoreTxt.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //code for entity in eatable range detected here triggering Eat(The ediable entity)
+    }
+
+    public void Eat(GameObject id)
+    {
+        //code for time delayed eating maybe should be co-routine
+    }
+}
