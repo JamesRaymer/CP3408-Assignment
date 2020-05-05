6467a75dfc70d92b1d78973dea7ce08b37df23c2
JamesRaymer <james.raymer@my.jcu.edu.au>
2020-05-05 13:30:25 +1000

    add foodSpawn script

---
+++ Assets/Scripts/foodSpawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodSpawn : MonoBehaviour
+{
+
+    public GameObject[] foodTypesArray; //array holding food types
+    public float speed; //time food gets generated
+
+    void Start()
+    {
+
+        //run generate() on start then repeat every speed seconds
+        InvokeRepeating("Generate", 0, speed);
+    }
+    //this generates food in the camera's view
+    void Generate()
+    {
+
+        //choose food type from Array
+        var i = Random.Range(0, foodTypesArray.Length);
+
+        //location chosen randomly on the screen in pixels
+        int x = Random.Range(0, Camera.main.pixelWidth);
+        int y = Random.Range(0, Camera.main.pixelHeight);
+
+        //convert pixel position to transform co-ordinates
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+
+        //x and y positions are set, set z to 0 for 2D Vector3
+        target.z = 0;
+
+        //Spawn food at target position with no rotation
+        Instantiate(foodTypesArray[i], target, Quaternion.identity);
+    }
+}
